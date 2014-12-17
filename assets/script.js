91fd12c79ea92b910c30014087aaf30bd5f707c6
Benedikt Forchhammer <b.forchhammer@gmail.com>
2014-12-17 12:19:18 +0100

    Removed console.debug() statement.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 (function ($) {
   $(document).on('leaflet.features', function (e, initial, leaflet) {
-    console.debug(leaflet);
     if (initial && leaflet.map_definition.plugins.hash) {
       new L.Hash(leaflet.lMap);
     }
